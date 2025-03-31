9e77f7877e867b99b368343c7666fcdc8248cf6f
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-03-31 10:35:40 +0200

    test: remove some tests

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -13,12 +13,12 @@ class PasswordResetTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_reset_password_link_screen_can_be_rendered(): void
-    {
-        $response = $this->get('/forgot-password');
-
-        $response->assertStatus(200);
-    }
+    // public function test_reset_password_link_screen_can_be_rendered(): void
+    // {
+    //     $response = $this->get('/forgot-password');
+    //
+    //     $response->assertStatus(200);
+    // }
 
     public function test_reset_password_link_can_be_requested(): void
     {
@@ -33,24 +33,24 @@ class PasswordResetTest extends TestCase
         Notification::assertSentTo($user, ResetPassword::class);
     }
 
-    public function test_reset_password_screen_can_be_rendered(): void
-    {
-        Notification::fake();
-
-        $user = User::factory()->create();
-
-        Volt::test('auth.forgot-password')
-            ->set('email', $user->email)
-            ->call('sendPasswordResetLink');
-
-        Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
-            $response = $this->get('/reset-password/'.$notification->token);
-
-            $response->assertStatus(200);
-
-            return true;
-        });
-    }
+    // public function test_reset_password_screen_can_be_rendered(): void
+    // {
+    //     Notification::fake();
+    //
+    //     $user = User::factory()->create();
+    //
+    //     Volt::test('auth.forgot-password')
+    //         ->set('email', $user->email)
+    //         ->call('sendPasswordResetLink');
+    //
+    //     Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
+    //         $response = $this->get('/reset-password/'.$notification->token);
+    //
+    //         $response->assertStatus(200);
+    //
+    //         return true;
+    //     });
+    // }
 
     public function test_password_can_be_reset_with_valid_token(): void
     {
