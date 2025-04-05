bd1c750e5b5f03327db2516f80cf9e9edcbbf9af
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-05 08:38:31 +0000

    refactor(api): add api prefix to route names

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\StickerApiController;
 use App\Http\Controllers\TagApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('stickers', StickerApiController::class)->only(['index', 'store', 'show']);
-Route::get('tags/tree', [TagApiController::class, 'tree']);
-Route::resource('tags', TagApiController::class)->only(['index', 'show']);
+Route::name('api.')->group(function () {
+    Route::resource('stickers', StickerApiController::class)->only(['index', 'store', 'show']);
+    Route::get('tags/tree', [TagApiController::class, 'tree'])->name('tags.tree');
+    Route::resource('tags', TagApiController::class)->only(['index', 'show']);
+});
+
