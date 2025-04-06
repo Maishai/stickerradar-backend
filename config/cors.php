8fd46b69a1c58335c97eef712e9b145ecf6a9ca4
Boolean-true <i22014@hb.dhbw-stuttgart.de>
2025-04-06 09:27:37 +0200

    fix(cors): add localhost:4200 to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('APP_URL').':8000', 'https://'.env('PRODUCTION_URL')],
+    'allowed_origins' => [env('APP_URL').':4200', env('APP_URL').':8000', 'https://'.env('PRODUCTION_URL')],
 
     'allowed_origins_patterns' => [],
 
