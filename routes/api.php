1747aced013015ccad6bf4732f9097a5d486e12e
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-06 23:28:30 +0200

    feat(rate-limiting): add basic rate limiting for api routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\StickerApiController;
 use App\Http\Controllers\TagApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('api.')->group(function () {
-    Route::resource('stickers', StickerApiController::class)->only(['index', 'store', 'show']);
+Route::name('api.')->middleware(['throttle:api'])->group(function () {
+    Route::resource('stickers', StickerApiController::class)->only(['index', 'show']);
+    Route::post('stickers', [StickerApiController::class, 'store'])->middleware(['throttle:sticker-upload'])->name('api.stickers.store');
     Route::get('tags/tree', [TagApiController::class, 'tree'])->name('tags.tree');
     Route::resource('tags', TagApiController::class)->only(['index', 'show']);
 });
