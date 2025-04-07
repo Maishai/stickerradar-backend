8a898ba2dc7769505078cbbef25d5cda56be1438
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-07 23:08:42 +0200

    fix(api): remove name duplication

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->middleware(['throttle:api'])->group(function () {
     Route::resource('stickers', StickerApiController::class)->only(['index', 'show']);
-    Route::post('stickers', [StickerApiController::class, 'store'])->middleware(['throttle:sticker-upload'])->name('api.stickers.store');
+    Route::post('stickers', [StickerApiController::class, 'store'])->middleware(['throttle:sticker-upload'])->name('stickers.store');
     Route::get('tags/tree', [TagApiController::class, 'tree'])->name('tags.tree');
     Route::resource('tags', TagApiController::class)->only(['index', 'show']);
 });
