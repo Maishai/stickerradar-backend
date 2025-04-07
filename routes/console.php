255d08b6500f6decb5ca04549b4f7849c0ec2495
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-07 13:50:14 +0200

    feat(monitoring): add laravel telescope

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use App\Console\Commands\GenerateMissingThumbnails;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(GenerateMissingThumbnails::class)->everyFiveMinutes()->appendOutputTo(storage_path('logs/schedule.log'));
+Schedule::command('telescope:prune --hours=96')->daily();
