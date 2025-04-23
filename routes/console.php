04d4b0acca66c5c0a1fac3b6af6c8d4d939691f2
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-23 22:03:46 +0200

    feat(backup): schedule daily backup

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(GenerateMissingThumbnails::class)->everyFiveMinutes()->appendOutputTo(storage_path('logs/schedule.log'));
 Schedule::command('telescope:prune --hours=96')->daily();
+Schedule::command('backup:run')->daily()->appendOutputTo(storage_path('logs/schedule.log'));
+Schedule::command('backup:clean')->daily()->appendOutputTo(storage_path('logs/schedule.log'));
