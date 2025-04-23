76bf9da665e8445a4a4dea367a478b2d0a519e0d
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-04-23 21:02:34 +0200

    fix(state-history): add factory and uuid traits

--- app/Models/StateHistory.php
+++ app/Models/StateHistory.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use App\State;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StateHistory extends Model
 {
+    use HasFactory;
+    use HasUuids;
+
     protected $fillable = ['sticker_id', 'state', 'last_seen'];
 
     protected $casts = [
